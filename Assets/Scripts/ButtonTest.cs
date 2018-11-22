fa4a74702368927f172a0e563b240dad93983ab1
ZhuoChengUCSD <35412729+ZhuoChengUCSD@users.noreply.github.com>
2018-11-21 16:12:46 -0800

    First development

---
+++ Assets/Scripts/ButtonTest.cs
@@ -0,0 +1,59 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using HoloToolkit.Unity;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.Receivers;
+using HoloToolkit.Unity.InputModule;
+using UnityEngine.UI;
+
+namespace HoloToolkit.Unity.Examples
+{
+    public class ButtonTest : InteractionReceiver
+    {
+        public GameObject textObjectState;
+        private TextMesh txt;
+
+        void Start()
+        {
+            txt = textObjectState.GetComponentInChildren<TextMesh>();
+        }
+
+        protected override void FocusEnter(GameObject obj, PointerSpecificEventData eventData)
+        {
+            if (obj == null) return;
+            Debug.Log(obj.name + " : FocusEnter");
+            txt.text = obj.name + " : FocusEnter";
+            obj.GetComponentInChildren<TextMesh>().text = "Test!";
+        }
+
+        protected override void FocusExit(GameObject obj, PointerSpecificEventData eventData)
+        {
+
+            if (obj == null) return;
+            Debug.Log(obj.name + " : FocusExit");
+            txt.text = obj.name + " : FocusExit";
+            obj.GetComponentInChildren<TextMesh>().text = "Test!";
+
+        }
+
+        protected override void InputDown(GameObject obj, InputEventData eventData)
+        {
+
+            if (obj == null) return;
+            obj.GetComponentInChildren<TextMesh>().text = "Test!";
+            Debug.Log(obj.name + " : InputDown");
+            txt.text = obj.name + " : InputDown";
+        }
+
+        protected override void InputUp(GameObject obj, InputEventData eventData)
+        {
+            Debug.Log(obj.name + " : InputUp");
+            txt.text = obj.name + " : InputUp";
+            obj.GetComponentInChildren<TextMesh>().text = "Test!";
+    
+
+        }
+    }
+}
---
+++ Assets/Scripts/ButtonTest2.cs
@@ -0,0 +1,48 @@
+﻿// To use this example, attach this script to an empty GameObject.
+// Create three buttons (Create>UI>Button). Next, select your
+// empty GameObject in the Hierarchy and click and drag each of your
+// Buttons from the Hierarchy to the Your First Button, Your Second Button
+// and Your Third Button fields in the Inspector.
+// Click each Button in Play Mode to output their message to the console.
+// Note that click means press down and then release.
+
+using UnityEngine;
+using UnityEngine.UI;
+using HoloToolkit.Unity;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.Receivers;
+using HoloToolkit.Unity.InputModule;
+using UnityEngine.UI;
+
+public class ButtonTest2 : MonoBehaviour
+{
+    //Make sure to attach these Buttons in the Inspector
+    public Button obj;
+
+    void Start()
+    {
+        //Calls the TaskOnClick/TaskWithParameters/ButtonClicked method when you click the Button
+        obj.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        //Output this to console when Button1 or Button3 is clicked
+        obj.GetComponentInChildren<Text>().text = "Who's your daddy!";
+
+        Debug.Log("You have clicked the button!");
+    }
+
+    void TaskWithParameters(string message)
+    {
+        //Output this to console when the Button2 is clicked
+        Debug.Log(message);
+    }
+
+    void ButtonClicked(int buttonNo)
+    {
+        //Output this to console when the Button3 is clicked
+        Debug.Log("Button clicked = " + buttonNo);
+    }
+}
